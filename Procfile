e2236716b8b4202d3f3351b0e07b95d6cd28a9eb
NetSHELL-Team <76968186+NetSHELL-Team@users.noreply.github.com>
2021-03-10 23:08:42 +0530

    ja apna km kr

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m Yui_GBot
+web: python3 -m Yui_GBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Yui_GBot Robot
+:: Enables virtual env mode and then starts Yui_GBot
+env\scripts\activate.bat && py -m Yui_GBot
